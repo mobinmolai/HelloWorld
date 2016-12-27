4df4b5cc2ac7a804d64e024e686b6e9a59be72f2
Mobin Molai <mobin18@gmail.com>
2016-12-27 17:51:05 +0500

    New changes

--- src/HelloWorld/wwwroot/_assets/js/site.js
+++ src/HelloWorld/wwwroot/_assets/js/site.js
@@ -4,8 +4,10 @@
         var $sidebarAndWrapper = $("#sidebar,#wrapper");
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar"))
-            $(this).text("Show Sidebar");
+            //$(this).text("Show Sidebar");
+            $(".sidebarToggle i.fa").removeClass("fa-angle-left").addClass("fa-angle-right");
         else
-            $(this).text("Hide Sidebar");
+            //$(this).text("Hide Sidebar");
+            $(".sidebarToggle i.fa").removeClass("fa-angle-right").addClass("fa-angle-left");
     });
 })();
